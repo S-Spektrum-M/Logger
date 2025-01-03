826a5117bdea6731133bed4765f3afab37081c85
Siddharth Mohanty <neosiddharth+git@gmail.com>
2025-01-03 08:38:43 -0800

    feat: Source_int

--- BasicSources.hpp
+++ BasicSources.hpp
@@ -15,4 +15,17 @@ template <StrConv T> class Source : public ISource {
         return ret;
     }
 };
+
+class Source_int : public ISource {
+    int val;
+
+public:
+    Source_int(int val) : val(val) {}
+    operator std::string() {return std::to_string(val);}
+    template <typename... Args> static Source_int *make(Args... args) {
+        Source_int *ret;
+        ret = new Source_int(args...);
+        return ret;
+    }
+};
 }
